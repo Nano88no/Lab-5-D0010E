8fdce8113e22853e9f0b960d5cd3dbdd63099dc0
Nano88no <100220633+Nano88no@users.noreply.github.com>
2022-03-13 10:53:52 +0100

    Update Event.java

--- lab/src/lab5/events/Event.java
+++ lab/src/lab5/events/Event.java
@@ -5,14 +5,25 @@ public abstract class Event {
 	private State state;
 	private EventQueue queue;
 	private double time;
+	/**
+	*@param state State som användas av simolatorn
+	*@param time Tid som Eventet inträffar
+	*@param queue EEventQueue som används
+	*/
 	public Event(State state,EventQueue queue,double time) {
 		this.state=state;
 		this.time=time;
 		this.queue=queue;
 	}
+	/**
+	 * @return time tiden för händelsen
+	 */
 	public double getTime() {
 		return time;
 	}
+	/**
+	 * utför effekten
+	 */
 	public abstract void effect();
 
 
